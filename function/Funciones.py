5ed39948436b8958a4748065d8520d9006b9d941
Arturo Sanchez <arturo66cta@gmail.com>
2022-09-26 12:35:49 -0300

    add product to reflectivity levels

--- function/Funciones.py
+++ function/Funciones.py
@@ -1,6 +1,7 @@
 
 import os
 import requests
+import numpy as np
 from tqdm import tqdm
 from bs4 import BeautifulSoup
 
@@ -42,3 +43,28 @@ def download_file(url_file,file_out):
         for data in tqdm(iterable=res.iter_content(chunk_size=1024), total=total_size/1024, unit='KB'):
             handle.write(data)
     # print("Done!")
+
+def dbz2mm(serie_in):
+    # convert dbz to mm with marshall-palmer relation
+    # R = (10**(Z/10)/200)**0.625
+    # Z = 10*np.log10(R**0.625*200)
+    lvl = len(serie_in)
+    serie_out = np.zeros(lvl)
+    for n in range(lvl):
+        if serie_in[n] == 0:
+            serie_out[n] = 0
+        else:
+            # serie_out[n] = ((10**(serie_in[n]/10))/200)**0.625
+            serie_out[n] = 10**(serie_in[n]/10)
+    return serie_out
+
+def mm2dbz(serie_in):
+    lvl = len(serie_in)
+    serie_out = np.zeros(lvl)
+    for n in range(lvl):
+        if serie_in[n] == 0:
+            serie_out[n] = 0
+        else:
+            # serie_out[n] = 10*np.log10((serie_in[n]*200)**0.625)
+            serie_out[n] = 10*np.log10(serie_in[n])
+    return serie_out
